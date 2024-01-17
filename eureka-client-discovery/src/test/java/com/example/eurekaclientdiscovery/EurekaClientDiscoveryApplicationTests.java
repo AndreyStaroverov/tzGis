d48596069ac7d8f838b99390df07f9293377c34d
Andrey <fanatSai@yandex.ru>
2024-01-18 00:59:46 +0300

    eureka server+eureka client restfull

---
+++ eureka-client-discovery/src/test/java/com/example/eurekaclientdiscovery/EurekaClientDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaclientdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
